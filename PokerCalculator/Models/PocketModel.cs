833e2f3e2f723b80c076f1ad6bdd2fb04890d56a
Emerald <emerald.grosjean@gmail.com>
2024-09-05 02:51:01 +0900

    Remove unused code

--- PokerCalculator/Models/PocketModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace PokerEvalApi.Models;
-
-public class PocketModel
-{
-    [RegularExpression(Const.Patterns.PocketPattern)]
-    public string Pocket { get; set; }
-}
